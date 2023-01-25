431417e7983d0e1e2e769174eca28e5ed23b5449
officialyenum <oponechukwuyenum@gmail.com>
2023-01-25 18:42:23 +0100

    Feat: Auth-Jwt validation, decoration and guards

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
 
-@Controller('user')
-export class UserController {}
+@UseGuards(JwtGuard)
+@Controller('users')
+export class UserController {
+  @Get('me')
+  getMe(@GetUser() user: User, @GetUser('email') email: string) {
+    console.log({ email });
+    return {
+      user: user,
+    };
+  }
+}
